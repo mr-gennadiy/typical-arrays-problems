cb08aa80653a7d92bc488200ad713dbefbcb154b
mr-gennadiy <mr.gennadiy@i.ua>
2020-09-29 23:31:52 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (array != undefined && array.length != 0) {
+    return array.sort((a, b) => a - b )[0];
+  } else return 0;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (array != undefined && array.length != 0) {
+    return array.sort((a, b) => b - a )[0];
+  } else return 0;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (array != undefined && array.length != 0) {
+    return array.reduce((sum, current) => sum + current, 0)/array.length;
+  } else return 0;
+};
